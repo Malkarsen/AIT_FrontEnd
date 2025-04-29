0c6f0d5602f0233cdd4e78eea4b30920497efa78
Malkarsen <malko.arina212@gmail.com>
2025-04-29 18:55:49 +0200

    JavaScript_introduction _ homework _ minor modification

--- homeworks/homework_08_JS/index.js
+++ homeworks/homework_08_JS/index.js
@@ -6,3 +6,6 @@ console.log(firstName);
 
 let isStudent = true;
 console.log(isStudent);
+
+// вывести все переменные вместе
+console.log("1 переменная: ", age, "2 переменная: ", firstName, "3 переменная: ", isStudent);
