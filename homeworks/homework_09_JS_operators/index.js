3e2fc49e6fef21e0ebf2b483e4f07923e86e0e10
Malkarsen <malko.arina212@gmail.com>
2025-05-06 19:27:10 +0200

    JS_Operators _ homework _ minor modification

--- homeworks/homework_09_JS_operators/index.js
+++ homeworks/homework_09_JS_operators/index.js
@@ -1,11 +1,33 @@
-let temperature = prompt("What's the temperature now?", 23);
+let temperature = prompt(
+  "Пожалуйста, введите температуру воздуха в вашем городе",
+  23
+);
 
-if (temperature < 0) {
-  console.log("Холодно!");
-} else if (0 <= temperature && temperature <= 20) {
-  console.log("Прохладно");
-} else if (21 <= temperature && temperature <= 30) {
-  console.log("Тепло");
+let normalizedTemperatire = Number(temperature);
+
+console.log(temperature);
+console.log(normalizedTemperatire);
+console.log(isNaN(normalizedTemperatire));
+
+if (temperature !== null) {
+  if (!isNaN(normalizedTemperatire) && temperature !== "") {
+    if (normalizedTemperatire < 0) {
+      console.log("Холодно!");
+    } else if (0 <= normalizedTemperatire && normalizedTemperatire <= 20) {
+      console.log("Прохладно");
+    } else if (21 <= normalizedTemperatire && normalizedTemperatire <= 30) {
+      console.log("Тепло");
+    } else if (normalizedTemperatire > 30) {
+      console.log("Жарко!");
+    }
+  } else {
+    console.log(
+      "Вы ввели некорректные символы или не ввели ничегоЮ перезагрузите страницу и введите число"
+    );
+  }
 } else {
-  console.log("Жарко!");
+  // если пользователь нажал отмену
+  console.log(
+    "Вы отказались вводить данные, перезагрузите страницу и попробуйте ещё раз!"
+  );
 }
