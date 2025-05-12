cbb5ac0b63069360df8cf443ba0b9c086b74ece0
Malkarsen <malko.arina212@gmail.com>
2025-05-12 12:07:37 +0200

    JS_Array _ lesson _ minor modification

--- lessons/lesson_10_JS_Array/array/index.js
+++ lessons/lesson_10_JS_Array/array/index.js
@@ -36,16 +36,94 @@ console.log(typeof animal);
 
 // 5. Хранение различных типов данных в одном массиве
 const mixedArray = [
-    0,
-    "Hello",
-    true,
-    animal,
-    {id: "1", name: "John", age: 20},
+  0,
+  "Hello",
+  true,
+  animal,
+  { id: "1", name: "John", age: 20 },
 ];
 console.log(mixedArray);
 
 // 6. Методы массивов pop/push, shift/unshift
 const fruits = ["Apple", "Orange", "Cherry", "Pineapple"];
-// - push() - метод массива, который добавялет элемент в конец массива
+// - push() - метод массива, который добавляет элемент в конец массива
+fruits.push("Grape");
+console.log(fruits);
+// - pop() - удаляет элемент с конца массива(а ещё и возвращает в переменную удаленный элемент)
+let grape = fruits.pop();
+console.log(fruits, grape);
 
+// - shift() - метод массива, который добавляет элемент в начало массива
+fruits.shift("Pear");
+console.log(fruits);
+// - unshift() - удаляет элемент с начала массива(а ещё и возвращает в переменную удаленный элемент)
+let pear = fruits.unshift();
+console.log(fruits, pear);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Циклы
+// 1. for
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
+console.log(numbers[6]);
+console.log(numbers[7]);
+console.log(numbers[8]);
+
+// Пример: вывести все элементы массива через цикл for
+for (let index = 0; index < numbers.length; index++) {
+  // Тело цикла for
+  console.log(numbers[index]);
+}
+// Пример: вывести все элементы массива, но не выводить второй
+for (let index = 0; index < numbers.length; index++) {
+  if (index === 2) {
+    continue;
+  } else {
+    console.log(numbers[index]);
+  }
+}
+
+// Пример: вывести первые 3 элемента массива, а затем прервать цикл
+for (let index = 0; index < numbers.length; index++) {
+  if (index === 3) {
+    break;
+  } else {
+    console.log(numbers[index]);
+  }
+}
+
+/////////////////////////////////////////////////////
+// 2. while
+const users = [
+  "John Johnson",
+  "Harry Potter",
+  "Johny Depp",
+  "Brat Pitt",
+  "Homer Simpson",
+];
+
+let index = 0;
+while (index < users.length) {
+  console.log(users[index]);
+  index++;
+}
+
+/////////////////////////////////////////////////////
+// 3. do-while
+let number = 6;
+
+do {
+  console.log("Сработает в любом случае");
+} while (number < 6);
+
+// преобразование данных
+// Number() - преобразует в number
+// Boolean() - преобразует в boolean
+console.log(Boolean(0)); // false
+// String() - преобразует в string
+console.log(String("256")); // 256
