ccf872ffcdbc19ef19e3d4296000588059d02128
Malkarsen <malko.arina212@gmail.com>
2025-05-15 19:23:32 +0200

    JS_functions _ homework _ minor modification

--- homeworks/homework_11_JS_functions/index.js
+++ homeworks/homework_11_JS_functions/index.js
@@ -1,20 +1,50 @@
 // Задание 1
+// V1
 function largerNumber(number1, number2) {
   return number1 > number2 ? number1 : number2;
 }
+// V2
+const getBiggerNumber = (number1, number2) => {
+  if (number1 > number2) {
+    return number1;
+  } else if (number1 < number2) {
+    return number2;
+  } else {
+    return "Числа равны";
+  }
+};
+
 let number1 = 6;
 let number2 = 2;
+// V1
 let resultLargerNumber = largerNumber(number1, number2);
 console.log(`Из чисел ${number1} и ${number2} большее: ${resultLargerNumber}`);
+// V2
+let resultBiggerNumber = getBiggerNumber(number1, number2);
+console.log(resultBiggerNumber);
 
 // Задание 2
+// V1
 const getElementByIndex = (array, index) => array[index];
+// V2
+const findElementByIndex = (array, index) => {
+  if (array.length <= index || index < 0) {
+    return "Ошибка! Индекс не найден";
+  } else {
+    return array[index];
+  }
+};
+
 const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let index = 4;
+// V1
 let resultGetElementByIndex = getElementByIndex(numberArray, index);
 console.log(
   `В массиве ${numberArray} под index ${index} стоит ${resultGetElementByIndex}`
 );
+// V2
+let resultFindElementByIndex = findElementByIndex(numberArray, index);
+console.log(resultFindElementByIndex);
 
 // Задание 3
 function decode(weatherCode) {
@@ -52,6 +82,6 @@ function decode(weatherCode) {
 }
 const weatherCodeInterpretation = (weatherCode, decode) => {
   return decode(weatherCode);
-};
+}; // getWeatherInfoByCode
 let weatherCode = "SS";
 console.log(weatherCodeInterpretation(weatherCode, decode));
