3676c57b065005c5bc79981d872c80a747d38bcf
Malkarsen <malko.arina212@gmail.com>
2025-05-22 19:44:17 +0200

    JS_DOM _ homework _ minor modification

--- homeworks/homework_12_JS_DOM/index.js
+++ homeworks/homework_12_JS_DOM/index.js
@@ -1,3 +1,6 @@
+const FORM = document.querySelector(".form");
+const IDEAL_WEIGHT_LABEL = document.querySelector(".ideal_weight_label");
+
 const calculatWeightOfWomen = (height) => {
   return height - (100 + (height - 100) / 10);
 };
@@ -9,20 +12,20 @@ const calculatWeightOfMen = (height) => {
 const calculatIdealWeight = (height, gender) => {
   if (gender === "male") {
     return calculatWeightOfMen(height);
-  } else {
+  } else if (gender === "female") {
     return calculatWeightOfWomen(height);
+  } else {
+    return "Ошибка в рассчетах";
   }
 };
-
-const FORM = document.querySelector(".form");
-const IDEAL_WEIGHT_LABEL = document.querySelector(".ideal_weight_label");
 
 FORM.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const height = event.target.height_form_input.value;
-  const gender = event.target.gender_form_input.value;
+  const height = event.target.height.value;
+  const gender = event.target.gender.value;
+  const normalizedHeight = Number(height);
 
-  const weight = calculatIdealWeight(height, gender);
+  const weight = calculatIdealWeight(normalizedHeight, gender);
   IDEAL_WEIGHT_LABEL.textContent = `Ваш идеальный вес: ${weight} кг`;
 });
