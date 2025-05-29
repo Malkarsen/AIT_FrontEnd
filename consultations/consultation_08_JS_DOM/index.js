e4ba3d42f14b5c36aacfb0e1c692eed817f1e586
Malkarsen <malko.arina212@gmail.com>
2025-05-29 07:55:31 +0200

    JS_DOM _ consultation _ completing the task

--- consultations/consultation_08_JS_DOM/index.js
+++ consultations/consultation_08_JS_DOM/index.js
@@ -1,7 +1,29 @@
 const TODO_LIST_FORM = document.querySelector(".todo_list_form");
+const TODO_ORDER_LIST = document.querySelector(".todo_order_list");
 
 TODO_LIST_FORM.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
+  const TODO_ORDER_LIST_ELEMENTS =
+    document.querySelectorAll(".new_task_element");
+
+  if (TODO_ORDER_LIST_ELEMENTS.length < 10) {
     const new_task = event.target.new_task.value;
+
+    if (new_task !== "") {
+      let newTaskElement = document.createElement("li");
+      newTaskElement.className = "new_task_element";
+      newTaskElement.textContent = new_task;
+      newTaskElement.addEventListener("click", () => {
+        newTaskElement.remove();
+      });
+
+      TODO_ORDER_LIST.append(newTaskElement);
+
+    } else {
+      alert("The task is empty!");
+    }
+  } else {
+    alert("You can only 10 items enter");
+  }
 });
