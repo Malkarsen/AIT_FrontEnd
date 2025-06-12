dbeccd69fadd2264895245d8680176ee9812800b
Malkarsen <malko.arina212@gmail.com>
2025-06-12 17:34:32 +0200

    JS_async-await_fetch_part_2 _ lesson

--- lessons/lesson_18_JS_Fetch/dog/index.js
+++ lessons/lesson_18_JS_Fetch/dog/index.js
@@ -1,22 +1,22 @@
-// fetch('https://dog.ceo/api/breeds/image/random')
-//   .then((response) => response.json)
-//   .then((obj) => {
-//     const { message } = obj;
-//     const dogImageElement = document.querySelector("#dog-image");
-//     console.log(dogImageElement);
-//     console.log(message);
-
-//     dogImageElement.src = message;
-//     dogImageElement.style.width = "500px";
-//   });
-
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch('https://dog.ceo/api/breeds/image/random')
   .then((response) => response.json())
   .then((obj) => {
     const { message } = obj; // деструктуризация
-    dogImageElement = document.getElementById("dog-image");
+    const dogImageElement = document.querySelector("#dog-image");
     console.log(dogImageElement);
     console.log(message);
+
     dogImageElement.src = message;
     dogImageElement.style.width = "500px";
   });
+
+// fetch("https://dog.ceo/api/breeds/image/random")
+//   .then((response) => response.json())
+//   .then((obj) => {
+//     const { message } = obj; 
+//     dogImageElement = document.getElementById("dog-image");
+//     console.log(dogImageElement);
+//     console.log(message);
+//     dogImageElement.src = message;
+//     dogImageElement.style.width = "500px";
+//   });
